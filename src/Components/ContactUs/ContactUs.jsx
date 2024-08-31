a0dcb6d7a1fdf06f9928d6a1993b22908326105d
yuvaraj-hacker <yuvarajtcz01@gmail.com>
2024-08-31 16:02:36 +0530

    change

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -62,8 +62,8 @@ function ContactUs() {
                </div>
               
             </div>
-            <div >
-            <iframe  className=" map  " src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3312.8844631949823!2d-117.55050800314665!3d33.86686832749506!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2sin!4v1725017305347!5m2!1sen!2sin" width="600" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <div  className="mapper "     >
+            <iframe  className=" h-full w-full " src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3312.8844631949823!2d-117.55050800314665!3d33.86686832749506!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2sin!4v1725017305347!5m2!1sen!2sin" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
            
          </div>
--- src/Components/Transition/Transition.jsx
+++ src/Components/Transition/Transition.jsx
@@ -6,7 +6,7 @@ const Transition = (OgComponent) => {
       initial={{ opacity: 0.15 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0.15 }}
-      transition={{ duration: 0.4, ease: [0.42, 0, 0.58, 1] }}
+      transition={{ duration: 0.2, ease: [0.42, 0, 0.58, 1] }}
     >
       <OgComponent />
     </motion.div>
